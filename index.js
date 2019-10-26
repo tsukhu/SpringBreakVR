5ea48fc167ef5a6f4ab117f2444c93748172461c
Tarun Kumar Sukhu - ERS <tsukhu@hcl.com>
2019-10-26 07:09:45 +0530

    update video360

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   AppRegistry,
   asset,
@@ -6,21 +6,24 @@ import {
   NativeModules,
   StyleSheet,
   Text,
-  View,
-} from 'react-360';
-import InfoButton from './components/InfoButton';
-import Promo from './components/Promo';
-import RockStarBeach from './components/RockStarBeach';
+  View
+} from "react-360";
+import InfoButton from "./components/InfoButton";
+import Promo from "./components/Promo";
+import RockStarBeach from "./components/RockStarBeach";
+import Events from "./components/Events";
 
-const SCENES = ['Promo', 'Events', 'Rockstar Beach'];
+const SCENES = ["Promo", "Events", "Rockstar Beach"];
 
 class Scene extends React.Component {
   state = {
-    scene: ''
-  }
+    scene: ""
+  };
 
   componentDidMount() {
-    Environment.setBackgroundImage(asset('chilling.jpg'), { rotateTransform: [{rotateY: '180deg'}] });
+    Environment.setBackgroundImage(asset("chilling.jpg"), {
+      rotateTransform: [{ rotateY: "180deg" }]
+    });
   }
 
   clearMedia() {
@@ -30,7 +33,7 @@ class Scene extends React.Component {
   clickHandler(selection) {
     this.setState({
       scene: selection
-    })
+    });
 
     this.clearMedia();
   }
@@ -40,10 +43,12 @@ class Scene extends React.Component {
     let selection;
     const sceneButtons = [];
 
-    if (scene === 'Promo') {
+    if (scene === "Promo") {
       selection = <Promo />;
-    } else if (scene === 'Rockstar Beach') {
-      selection = <RockStarBeach />
+    } else if (scene === "Rockstar Beach") {
+      selection = <RockStarBeach />;
+    } else if (scene === "Events") {
+      selection = <Events />;
     }
 
     for (let i in SCENES) {
@@ -51,22 +56,21 @@ class Scene extends React.Component {
         <InfoButton
           key={i}
           style={styles.button}
-          source={asset('palm-tree.png')}
+          source={asset("palm-tree.png")}
           text={SCENES[i]}
-          onClick={() => { this.clickHandler(SCENES[i])}}
-        />)
+          onClick={() => {
+            this.clickHandler(SCENES[i]);
+          }}
+        />
+      );
     }
 
-    return(
+    return (
       <View style={styles.panel}>
-        <View>
-          {selection}
-        </View>
-        <View style={styles.section}>
-          {sceneButtons}
-        </View>
+        <View>{selection}</View>
+        <View style={styles.section}>{sceneButtons}</View>
       </View>
-    )
+    );
   }
 }
 
@@ -74,21 +78,21 @@ const styles = StyleSheet.create({
   panel: {
     width: 800,
     height: 450,
-    flexDirection: 'column',
-    justifyContent: 'flex-end',
+    flexDirection: "column",
+    justifyContent: "flex-end"
   },
   section: {
     padding: 10,
-    backgroundColor: '#FFE500',
-    borderColor: '#ED8B00',
+    backgroundColor: "#FFE500",
+    borderColor: "#ED8B00",
     borderWidth: 5,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
+    flexDirection: "row",
+    justifyContent: "space-around"
   },
   button: {
     marginLeft: 5,
-    marginRight: 5,
+    marginRight: 5
   }
 });
 
-AppRegistry.registerComponent('Scene', () => Scene);
+AppRegistry.registerComponent("Scene", () => Scene);
